eaf39de11208fcdfbba84fc50b01589876df510f
brsklar <brskl@msn.com>
2017-04-18 18:57:26 -0700

    Initial commit

---
+++ app/src/main/java/com/benjaminsklar/myandroid310/MainActivity.java
@@ -0,0 +1,25 @@
+package com.benjaminsklar.myandroid310;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.uxVolley)
+    public void onVolley() {
+        Intent intent = new Intent(this, VolleyActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/benjaminsklar/myandroid310/VolleyActivity.java
@@ -0,0 +1,84 @@
+package com.benjaminsklar.myandroid310;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class VolleyActivity extends AppCompatActivity {
+
+    @Bind(R.id.uxTextView)
+    public TextView mTextView;
+
+    @Bind(R.id.uxImageView)
+    ImageView mImageView;
+
+    @Bind(R.id.uxNetworkImageView)
+    NetworkImageView mNetworkImageView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volley);
+
+        ButterKnife.bind(this);
+
+        // Instantiate the RequestQueue.
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        requestQueue.add(stringRequest);
+
+
+        url = "http://i.imgur.com/7spzG.png";
+
+        // Retrieves an image specified by the URL, displays it in the UI.
+        ImageRequest imageRequest = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        mImageView.setImageBitmap(bitmap);
+                    }
+                }, 0, 0, null,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        mImageView.setImageResource(0);
+                    }
+                });
+
+        requestQueue.add(imageRequest);
+        
+    }
+}
